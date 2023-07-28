d26747792d395411107d47bfcc95a611fec6fb54
Elisei-26 <pralaelisei76@gmail.com>
2023-07-28 16:00:58 +0300

    third changes requested solved

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,6 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConfigModule } from "@nestjs/config";
-import { UserService } from "./user/user.service";
-import { User } from "./user/user.entity";
-import { UserMapper } from "./user/user.mapper";
-import { UserController } from "./user/user.controller";
 import { UserModule } from "./user/user.module";
 
 @Module({
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,11 +2,11 @@ import { Controller, Get } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { User } from "./user.entity";
 
-@Controller("users")
+@Controller("user")
 export class UserController {
 	constructor(private readonly userService: UserService) {}
 
-	@Get()
+	@Get("all")
 	async findAll(): Promise<User[]> {
 		return this.userService.findAll();
 	}
