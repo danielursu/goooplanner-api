5aa67c9601852fc1ad223dab459c52955389ce55
Victor Purice <victor.purice@ulbsibiu.ro>
2024-08-01 23:19:26 +0300

    change requests solved, created new migration for users table

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,10 +1,20 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, Unique, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
+@Unique(["email"])
 export class User {
 	@PrimaryGeneratedColumn()
 	id: number;
 
 	@Column("text", { name: "first_name" })
 	firstName: string;
+
+	@Column("text", { name: "last_name" })
+	lastName: string;
+
+	@Column("text", { name: "email" })
+	email: string;
+
+	@Column("text", { name: "password" })
+	password: string;
 }
--- src/user/user.mapper.ts
+++ src/user/user.mapper.ts
@@ -17,6 +17,9 @@ export class UserMapper {
 		return {
 			id: user.id,
 			firstName: user.firstName,
+			lastName: user.lastName,
+			email: user.email,
+			password: user.password,
 		};
 	}
 }
