71e4cdd19fbb73534874ab08feef31c7a03df6c9
Victor Purice <victor.purice@ulbsibiu.ro>
2024-08-18 00:58:38 +0300

    set the refresh_token in the httpOnly cookie

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,18 @@
-import { Controller, UseGuards, Post, Get, Req, Body } from "@nestjs/common";
-import { Request } from "express";
+import { Controller, UseGuards, Post, Get, Req, Body, Res } from "@nestjs/common";
+import { Request, Response } from "express";
+import { jwtDecode } from "jwt-decode";
 
 import { AuthService } from "./auth.service";
 import { LocalGuard } from "./guards/local.guard";
 import { JwtAuthGuard } from "./guards/jwt.guard";
 import { UserDTO } from "src/user/dto/user.dto";
+
+export interface User {
+	id: number;
+	username: string;
+	password: string;
+	refresh_token?: string;
+}
 
 @Controller("auth")
 export class AuthController {
@@ -12,15 +20,39 @@ export class AuthController {
 
 	@Post("login")
 	@UseGuards(LocalGuard)
-	async login(@Req() req: Request) {
-		console.log("hit auth.controller.ts login: ", req.user);
-		return req.user;
+	async login(@Req() req: Request, @Res() res: Response) {
+		// console.log("Request object: ", req.user);
+		// return req.user;
+
+		const user = req.user as User;
+		const refreshToken = user.refresh_token;
+
+		// calcular the expiration
+		const decodedToken: { exp: number } = jwtDecode(refreshToken);
+		const exp = decodedToken.exp;
+		const currentTime = Math.floor(Date.now() / 1000);
+		const maxAge = (exp - currentTime) * 1000;
+
+		res.cookie("refresh_token", refreshToken, {
+			httpOnly: false,
+			secure: false,
+			sameSite: "strict",
+			maxAge: maxAge,
+			path: "/",
+		});
+		console.log("Request object: ", req.user);
+		return res.json(req.user);
+	}
+
+	@Get("get-cookie")
+	getCookie(@Req() request: Request) {
+		const value = request.cookies["refresh_token"];
+		return value;
 	}
 
 	@Post("register")
 	async register(@Body() userDTO: UserDTO) {
 		const user = await this.authService.register(userDTO);
-		console.log(user);
 		return this.authService.login(user);
 	}
 
